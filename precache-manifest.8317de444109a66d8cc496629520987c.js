c9726a9519b3cc415ad3428010a4e215ce432559
indiraaridni <indiraaridni@mail.ru>
2020-07-24 02:44:08 -0400

    Updates

---
+++ precache-manifest.8317de444109a66d8cc496629520987c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a43e24c9b57fb9a54eb829702e2917b",
+    "url": "/callback_counters/index.html"
+  },
+  {
+    "revision": "55f99d6ceff707fc98e8",
+    "url": "/callback_counters/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5981e7413e00a05ba285",
+    "url": "/callback_counters/static/css/main.014146f8.chunk.css"
+  },
+  {
+    "revision": "55f99d6ceff707fc98e8",
+    "url": "/callback_counters/static/js/2.7f1a0dd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/callback_counters/static/js/2.7f1a0dd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5981e7413e00a05ba285",
+    "url": "/callback_counters/static/js/main.2fc9bb2b.chunk.js"
+  },
+  {
+    "revision": "0e5bbdd6c2dd1bc9c254",
+    "url": "/callback_counters/static/js/runtime-main.ea2c631a.js"
+  }
+]);
